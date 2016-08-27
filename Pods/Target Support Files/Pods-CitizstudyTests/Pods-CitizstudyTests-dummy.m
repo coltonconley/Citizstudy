8664bd408973b4b28a00607a554f8a835e4b1474
Colton Conley <coltonconley@gmail.com>
2016-08-27 16:05:20 -0700

    reset pods, deleted unneccesary files

---
+++ Pods/Target Support Files/Pods-CitizstudyTests/Pods-CitizstudyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CitizstudyTests : NSObject
+@end
+@implementation PodsDummy_Pods_CitizstudyTests
+@end
---
+++ Pods/Target Support Files/Pods-CitizstudyUITests/Pods-CitizstudyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CitizstudyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CitizstudyUITests
+@end
